3ce767b0caa19e4f0605f5dfc434304a9d93043e
JozefMikus <jozef.mikus@kyndryl.com>
2022-09-18 13:21:52 +0200

    -- init

---
+++ src/main/java/sk/jo2o/hexa/HexaApplication.java
@@ -0,0 +1,13 @@
+package sk.jo2o.hexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sk/jo2o/hexa/HexaApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.jo2o.hexa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
